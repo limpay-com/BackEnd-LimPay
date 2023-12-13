0195e3d1d4fc0b5e42bc708343aa2299f035221c
Luiz Felipe Melo dos Anjos <131801895+LuizFelipeMelo18@users.noreply.github.com>
2023-12-13 12:55:40 -0300

    Delete CadastroInicial/BackEnd-LimPay directory

--- CadastroInicial/BackEnd-LimPay/src/main/java/br/com/limpay/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package br.com.limpay.service;
-
-import br.com.limpay.domain.User;
-
-public interface UserService {
-    User registerUser(User user);
-
-    User findUserByEmail(String username);
-}
